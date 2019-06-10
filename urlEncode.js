2d26fa34e1cb5bc48319536d8de648fe2e5d97d5
spiritxhx <spiritxhx@gmail.com>
2019-06-10 14:12:21 -0400

    modify the urlEncode so it works properly now

--- urlEncode.js
+++ urlEncode.js
@@ -23,4 +23,4 @@ const urlEncode = text => {
 }
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode("     Lighthouse Labs   "));
-console.log(urlEncode("blue is greener than purple for sure"));
+console.log(urlEncode("    blue is greener than purple for sure               "));
