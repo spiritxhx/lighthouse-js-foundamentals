acc1c852144f5ea2ef90eac0c928b7b7dec09606
spiritxhx <spiritxhx@gmail.com>
2019-06-11 18:38:34 -0400

    add Kata6-repeatNumbers.js

---
+++ Kata/Kata6-repeatNumbers.js
@@ -0,0 +1,15 @@
+const repeatNumbers = data => {
+  let ans=[];
+  for (let i = 0; i< data.length; i++){
+    let tempNumber = "";
+    for (let j = 0; j< data[i][1]; j++){
+      tempNumber = `${tempNumber}${data[i][0]}`
+    }
+    ans[i] = tempNumber;
+  }
+  return ans;
+}
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
