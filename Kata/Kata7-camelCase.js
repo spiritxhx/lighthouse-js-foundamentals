548c2f4773ea4da2479b573a38cea004d916efb6
spiritxhx <spiritxhx@gmail.com>
2019-06-11 20:02:35 -0400

    add the camelCase.js

---
+++ Kata/Kata7-camelCase.js
@@ -0,0 +1,13 @@
+const camelCase = input => {
+  inputArr = input.split('');
+  for (let i = 0; i < inputArr.length; i++){
+    if (inputArr[i]===' '){
+      inputArr[i+1]=inputArr[i+1].toUpperCase();
+    }
+  }
+  let ans = inputArr.join('');
+  return ans;
+}
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
